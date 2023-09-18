0fa7c04081c4e747bbcd659cdc26ebbe132875ad
JOAO-P-COELHO <143346573+JOAO-P-COELHO@users.noreply.github.com>
2023-09-18 19:19:54 +0100

    none

--- jQuery/js.js
+++ jQuery/js.js
@@ -1,3 +1,19 @@
-$("#teste").click(function() {
+$("#h1").click(function() {
     $(this).hide();
 } );
+
+$( "#p1" ).hover(function() {
+    $( this ).hide( 500 );
+    })
+
+$( "#p2" ).click(function() {
+     $( this ).hide( 100 );
+    })
+
+  $( "#p3" ).dblclick(function() {
+    $( this ).hide( 100 );
+    })
+
+  $( "#p4" ).mouseout(function() {
+    $( this ).hide( 100 );
+       })
