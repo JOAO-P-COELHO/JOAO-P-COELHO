7f9bc3e7e1d510bd9fbcbc5681c644b6e5f4a1ae
JOAO-P-COELHO <143346573+JOAO-P-COELHO@users.noreply.github.com>
2023-09-19 15:06:19 +0100

    Update js_nav.js

--- NP_NavBars_with_info_boxes/js_nav.js
+++ NP_NavBars_with_info_boxes/js_nav.js
@@ -1,11 +1,21 @@
 $("#menu_img").click(function () { 
   $("#navigation").toggle(1000);
-  
- });
+});
 
  $(".info-content").hide(); 
 
  $(".info-title").click(function () { 
   $(".info-content").slideToggle(1000);
   
- });
+});
+
+$(".content-box").click(function(){
+  $(".content-before").animate({
+  background: 'blue'
+  }, 1000);
+});
+
+$(".content-box").click(function(){
+  $(".content-before").animate({background: 'blue'});
+});
+
